4cf385d45d58397e83c51dbf78935e23c5ba798e
RafaelMoid <rafaelvrcr@gmail.com>
2023-06-05 21:38:46 -0300

    page historico

---
+++ page-historico.php
@@ -0,0 +1,117 @@
+<?php
+/*
+  Template Name: Historico 
+*/
+?>
+
+<?php get_header(); ?>
+
+<main id="historico">
+    <section class="histo-top" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Historico.png);">
+        <h1>Credibilidade e Confiança</h1>
+        <div class="lines">
+            <div class="line1"></div>
+            <div class="line2"></div>
+        </div>
+        <h2>Produtos de qualidade e um mercado de Semi-Novos</br>
+            para trazer o melhor ao seu projeto ou serviço.</h2>
+    </section>
+    <section class="sobre-hcmaq">
+        <div class="img-wrapper" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Historico.png);"></div>
+        <div class="text-wrapper">
+            <h2>Sobre a HCMAQ</h2>
+            <div class="lines">
+                <div class="line1"></div>
+                <div class="line2"></div>
+            </div>
+            <p>Pedro Hugo Chaves Bastos, Brasileiro, casado, natural
+                de Recife-PE, 18/11/1986, atua no ramo de representação
+                comercial de guindauto desde 2008.
+                </br>
+                </br>
+                A princípio, preposto do representante à época Eric
+                da Masal (Masal S/A Ind. e Com. / Sto. Antônio da Patrulha
+                -RS) atuando nos estados de SE/AL/PE/PB/RN/CE/PI até
+                meados de 2015.
+                </br>
+                </br>
+                Já como representante, atuou na PHD (PHD IND. / Caxias
+                do Sul-RS) nos estados de AL/PE/PB/CE do final de 2015
+                a início de 2020. Meados de 2020 a data atual, atende
+                os estados de AL/PE/PB/CE pela TKA Guindastes
+                (TKA Guindastes / Flores da Cunha-RS).
+                </br>
+                </br>
+                Representante da Bristol Industria (Bristol Perfuratriz
+                e Brocas / São Jeronimo-RS) desde 2021. Distribuidor
+                da TER Brasil desde 2012 (TER Brasil Equipamentos
+                Hidráulicos (Bombas hidráulicas) / Caxias do Sul-RS)
+                e proprietário da HCMAQ Guindauto acessórios,
+                que disponde de uma diversa linha de acessórios
+                correlacionados ao guindauto (cestos aéreos, cabines
+                auxiliares, garras pega palet, guincho de cabo, cintas…)
+                estes, a pronta entrega em Jaboatão dos Guararapes - PE.</p>
+        </div>
+    </section>
+    <section class="missao" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/Historico2.png);">
+        <div>
+            <h3>Missão</h3>
+            <div class="lines">
+                <div class="line1"></div>
+                <div class="line2"></div>
+            </div>
+            <p>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed
+                do
+                eiusmod tempor incididunt
+                ut labore et dolore magna aliqua.
+                </br>
+                </br>
+
+                Quis ipsum suspendisse ultrices
+                gravida. Risus commodo viverra
+                maecenas accumsan lacus vel
+                facilisis. </p>
+        </div>
+        <div>
+            <h3>Visão</h3>
+            <div class="lines">
+                <div class="line1"></div>
+                <div class="line2"></div>
+            </div>
+            <p>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed
+                do
+                eiusmod tempor incididunt
+                ut labore et dolore magna aliqua.
+                </br>
+                </br>
+
+                Quis ipsum suspendisse ultrices
+                gravida. Risus commodo viverra
+                maecenas accumsan lacus vel
+                facilisis. </p>
+        </div>
+        <div>
+            <h3>Valores</h3>
+            <div class="lines">
+                <div class="line1"></div>
+                <div class="line2"></div>
+            </div>
+            <p>Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit, sed
+                do
+                eiusmod tempor incididunt
+                ut labore et dolore magna aliqua.
+                </br>
+                </br>
+
+                Quis ipsum suspendisse ultrices
+                gravida. Risus commodo viverra
+                maecenas accumsan lacus vel
+                facilisis. </p>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
