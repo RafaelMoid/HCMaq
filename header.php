49949a5dd0acd6e782e0010f36feb3ebe26cfc78
RafaelMoid <rafaelvrcr@gmail.com>
2023-07-06 14:56:45 -0300

    done

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Rafael Varela">
 
     <!-- Meta Tags para Open Graph  -->
     <!-- <meta property="og:image" content="<?php echo get_stylesheet_directory_uri(); ?>/wp-content/uploads/2023/04/fiabesa-og-image.png" />
